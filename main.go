8431d868ea7798009ac13ee450db25f62f33246b
Nishant Sharma <sushank123456@gmail.com>
2023-09-04 09:32:26 +0530

    add & show item function

--- main.go
+++ main.go
@@ -4,18 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
+
 var tasks []string
 
 func main(){
 	item := -1 
 	for item != 0 {
-
+		item = menu()
+		switch(item){
+		case 1 : 
+			showList()
+			break
+		case 2:
+			addItem()
+		case 3: 
+			removeItem()
+		case 0:
+			break
+		default:
+			fmt.Println("Enter a valid option")
+		}
 	}
 }
 
 func menu() int {
+	var input int
 	fmt.Println()
 	fmt.Println("----------------------")
 	fmt.Println("     Main Menu")
@@ -26,10 +40,34 @@ func menu() int {
 	fmt.Println("3. Remove item from list")
 	fmt.Println()
 	fmt.Println("Enter choice: ")
-	scanner := bufio.NewScanner(os.Stdin)
-	input, err := strconv.Atoi(scanner.Text())
-	if err != nil {
-		fmt.Println(err)
-	}
+	fmt.Scan(&input)
 	return input
+}
+
+func showList(){
+	fmt.Println()
+	fmt.Println("----------------------")
+	fmt.Println("     To-Do List")
+	fmt.Println("----------------------")
+	number := 0
+	for _, element := range tasks{
+		number = number + 1
+		fmt.Printf("%d  %s", number, element)
+	} 
+	fmt.Println()
+	fmt.Println("----------------------")
+}
+
+func addItem(){
+	fmt.Println()
+	fmt.Println("Add Item")
+	fmt.Println("----------------------")
+	fmt.Print("Enter an item: ")
+	reader := bufio.NewReader(os.Stdin)
+    userInput, _ := reader.ReadString('\n')
+	tasks = append(tasks, userInput)
+}
+
+func removeItem(){
+
 }
